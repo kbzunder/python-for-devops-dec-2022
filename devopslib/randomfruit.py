bf94ca328e559ffda07f227f57b6f192dad55695
kbzunder <kbzunder@gmail.com>
2022-12-04 06:57:09 +0000

    adding command-line-tool

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -6,5 +6,13 @@ def fruit():
     return choices(fruits)[0]
 
 
+def meal(beverage):
+    my_fruit = fruit()
+    print(f"your fruit is {my_fruit} and your beverage is {beverage}")
+    if my_fruit == 'cherry':
+        complete_meal = f"your meal is a {my_fruit} and {beverage}"
+        return complete_meal
+    return  f"your meal is a steak and {beverage}"
+
 # var = 1
 # var = var
--- test_devopslib.py
+++ test_devopslib.py
@@ -1,6 +1,9 @@
-from devopslib.randomfruit import fruit
-
+from devopslib.randomfruit import fruit, meal
 
 def test_fruit():
     fruit_choice = fruit()
     assert fruit_choice in ["apple", "cherry", "strawberry"]
+    
+def test_the_meal():
+    result = meal("milk")
+    assert "milk" in result
